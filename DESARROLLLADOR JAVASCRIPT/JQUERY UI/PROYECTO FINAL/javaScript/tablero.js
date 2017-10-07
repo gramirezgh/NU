97326a50fef91105cd606c4a030b82f473ea59fc
gramirezgh <guerynextu@gmail.com>
2017-10-07 01:36:27 -0400

    proyceto final cambio 2
    proyceto final cambio 2

--- DESARROLLLADOR JAVASCRIPT/JQUERY UI/PROYECTO FINAL/javaScript/tablero.js
+++ DESARROLLLADOR JAVASCRIPT/JQUERY UI/PROYECTO FINAL/javaScript/tablero.js
@@ -6,4 +6,191 @@ $(function () {
     });
 
     $("#pieza1").draggable();
+
+    $("#pieza2").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo2"
+    });
+
+    $("#pieza2").draggable();
+
+    $("#pieza3").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo3"
+    });
+
+    $("#pieza3").draggable();
+
+    $("#pieza4").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo4"
+    });
+
+    $("#pieza4").draggable();
+
+    $("#pieza5").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo5"
+    });
+
+    $("#pieza5").draggable();
+
+
+    $("#pieza6").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo6"
+    });
+
+    $("#pieza6").draggable();
+
+    $("#pieza7").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo7"
+    });
+
+    $("#pieza7").draggable();
+
+    $("#pieza8").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo8"
+    });
+
+    $("#pieza8").draggable();
+
+    $("#pieza9").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo9"
+    });
+
+    $("#pieza9").draggable();
+
+    $("#pieza10").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo10"
+    });
+
+    $("#pieza10").draggable();
+
+    $("#pieza11").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo11"
+    });
+
+    $("#pieza11").draggable();
+
+    $("#pieza12").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo12"
+    });
+
+    $("#pieza12").draggable();
+
+
+    $("#pieza13").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo13"
+    });
+
+    $("#pieza13").draggable();
+
+    $("#pieza14").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo14"
+    });
+
+    $("#pieza14").draggable();
+
+    $("#pieza15").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo15"
+    });
+
+    $("#pieza15").draggable();
+
+    $("#pieza16").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo16"
+    });
+
+    $("#pieza16").draggable();
+
+    $("#pieza17").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo17"
+    });
+
+    $("#pieza17").draggable();
+
+
+    $("#pieza18").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo18"
+    });
+
+    $("#pieza18").draggable();
+
+    $("#pieza19").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo19"
+    });
+
+    $("#pieza19").draggable();
+
+    $("#pieza20").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo20"
+    });
+
+    $("#pieza20").draggable();
+
+    $("#pieza21").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo21"
+    });
+
+    $("#pieza21").draggable();
+
+    $("#pieza22").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo22"
+    });
+
+    $("#pieza22").draggable();
+
+    $("#pieza23").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo23"
+    });
+
+    $("#pieza23").draggable();
+
+    $("#pieza24").position({
+        my:"center",
+        at:"center",
+        of:"#elementoObjetivo24"
+    });
+
+    $("#pieza24").draggable();
 });
