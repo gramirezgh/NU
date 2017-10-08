787cd8cd3ac09d44d3325ff81610a9e11d0b4328
gramirezgh <guerynextu@gmail.com>
2017-10-07 20:23:30 -0400

    proyecto final JQUERY-UI documentacion de codigo fuente
    proyecto final JQUERY-UI documentacion de codigo fuente

--- DESARROLLLADOR JAVASCRIPT/JQUERY UI/PROYECTO FINAL/javaScript/tablero.js
+++ DESARROLLLADOR JAVASCRIPT/JQUERY UI/PROYECTO FINAL/javaScript/tablero.js
@@ -1,4 +1,6 @@
 $(function () {
+// Posesionando las piezas por medio de POSITION
+    //Posesionando la pieza 1 en elementoObjetivo1 y la propiedad de movimiento
     $("#pieza1").position({
         my:"center",
         at:"center",
@@ -7,6 +9,7 @@ $(function () {
 
     $("#pieza1").draggable();
 
+    //Posesionando la pieza 2 en elementoObjetivo2 y la propiedad de movimiento
     $("#pieza2").position({
         my:"center",
         at:"center",
@@ -15,6 +18,7 @@ $(function () {
 
     $("#pieza2").draggable();
 
+    //Posesionando la pieza 3 en elementoObjetivo3 y la propiedad de movimiento
     $("#pieza3").position({
         my:"center",
         at:"center",
@@ -23,6 +27,7 @@ $(function () {
 
     $("#pieza3").draggable();
 
+    //Posesionando la pieza 4 en elementoObjetivo4 y la propiedad de movimiento
     $("#pieza4").position({
         my:"center",
         at:"center",
@@ -30,7 +35,7 @@ $(function () {
     });
 
     $("#pieza4").draggable();
-
+    //Posesionando la pieza 5 en elementoObjetivo5 y la propiedad de movimiento
     $("#pieza5").position({
         my:"center",
         at:"center",
@@ -39,7 +44,7 @@ $(function () {
 
     $("#pieza5").draggable();
 
-
+    //Posesionando la pieza 6 en elementoObjetivo6 y la propiedad de movimiento
     $("#pieza6").position({
         my:"center",
         at:"center",
@@ -47,7 +52,7 @@ $(function () {
     });
 
     $("#pieza6").draggable();
-
+    //Posesionando la pieza 7 en elementoObjetivo7 y la propiedad de movimiento
     $("#pieza7").position({
         my:"center",
         at:"center",
@@ -55,7 +60,7 @@ $(function () {
     });
 
     $("#pieza7").draggable();
-
+    //Posesionando la pieza 8 en elementoObjetivo8 y la propiedad de movimiento
     $("#pieza8").position({
         my:"center",
         at:"center",
@@ -63,7 +68,7 @@ $(function () {
     });
 
     $("#pieza8").draggable();
-
+    //Posesionando la pieza 9 en elementoObjetivo9 y la propiedad de movimiento
     $("#pieza9").position({
         my:"center",
         at:"center",
@@ -71,7 +76,7 @@ $(function () {
     });
 
     $("#pieza9").draggable();
-
+    //Posesionando la pieza 10 en elementoObjetivo10 y la propiedad de movimiento
     $("#pieza10").position({
         my:"center",
         at:"center",
@@ -79,7 +84,7 @@ $(function () {
     });
 
     $("#pieza10").draggable();
-
+    //Posesionando la pieza 11 en elementoObjetivo11 y la propiedad de movimiento
     $("#pieza11").position({
         my:"center",
         at:"center",
@@ -87,7 +92,7 @@ $(function () {
     });
 
     $("#pieza11").draggable();
-
+    //Posesionando la pieza 12 en elementoObjetivo12 y la propiedad de movimiento
     $("#pieza12").position({
         my:"center",
         at:"center",
@@ -96,7 +101,7 @@ $(function () {
 
     $("#pieza12").draggable();
 
-
+    //Posesionando la pieza 13 en elementoObjetivo13 y la propiedad de movimiento
     $("#pieza13").position({
         my:"center",
         at:"center",
@@ -104,7 +109,7 @@ $(function () {
     });
 
     $("#pieza13").draggable();
-
+    //Posesionando la pieza 14 en elementoObjetivo14 y la propiedad de movimiento
     $("#pieza14").position({
         my:"center",
         at:"center",
@@ -112,7 +117,7 @@ $(function () {
     });
 
     $("#pieza14").draggable();
-
+    //Posesionando la pieza 15 en elementoObjetivo15 y la propiedad de movimiento
     $("#pieza15").position({
         my:"center",
         at:"center",
@@ -120,7 +125,7 @@ $(function () {
     });
 
     $("#pieza15").draggable();
-
+    //Posesionando la pieza 16 en elementoObjetivo16 y la propiedad de movimiento
     $("#pieza16").position({
         my:"center",
         at:"center",
@@ -128,7 +133,7 @@ $(function () {
     });
 
     $("#pieza16").draggable();
-
+    //Posesionando la pieza 17 en elementoObjetivo17 y la propiedad de movimiento
     $("#pieza17").position({
         my:"center",
         at:"center",
@@ -137,7 +142,7 @@ $(function () {
 
     $("#pieza17").draggable();
 
-
+    //Posesionando la pieza 18 en elementoObjetivo18 y la propiedad de movimiento
     $("#pieza18").position({
         my:"center",
         at:"center",
@@ -146,6 +151,7 @@ $(function () {
 
     $("#pieza18").draggable();
 
+    //Posesionando la pieza 19 en elementoObjetivo19 y la propiedad de movimiento
     $("#pieza19").position({
         my:"center",
         at:"center",
@@ -154,6 +160,7 @@ $(function () {
 
     $("#pieza19").draggable();
 
+    //Posesionando la pieza 20 en elementoObjetivo20 y la propiedad de movimiento
     $("#pieza20").position({
         my:"center",
         at:"center",
@@ -162,6 +169,7 @@ $(function () {
 
     $("#pieza20").draggable();
 
+    //Posesionando la pieza 21 en elementoObjetivo21 y la propiedad de movimiento
     $("#pieza21").position({
         my:"center",
         at:"center",
@@ -170,6 +178,7 @@ $(function () {
 
     $("#pieza21").draggable();
 
+    //Posesionando la pieza 22 en elementoObjetivo22 y la propiedad de movimiento
     $("#pieza22").position({
         my:"center",
         at:"center",
@@ -178,6 +187,7 @@ $(function () {
 
     $("#pieza22").draggable();
 
+    //Posesionando la pieza 23 en elementoObjetivo23 y la propiedad de movimiento
     $("#pieza23").position({
         my:"center",
         at:"center",
@@ -186,6 +196,7 @@ $(function () {
 
     $("#pieza23").draggable();
 
+    //Posesionando la pieza 24 en elementoObjetivo24 y la propiedad de movimiento
     $("#pieza24").position({
         my:"center",
         at:"center",
@@ -193,4 +204,115 @@ $(function () {
     });
 
     $("#pieza24").draggable();
+
+    $("#pieza25").draggable();
+    $("#pieza26").draggable();
+    $("#pieza27").draggable();
+
+    $("#pieza28").position({
+        my:"left",
+        at:"right",
+        of:"#pieza25"
+    });
+
+    $("#pieza28").draggable();
+
+    //Posesionando la pieza 29 en pieza 26 y la propiedad de movimiento
+    $("#pieza29").position({
+        my:"left",
+        at:"right",
+        of:"#pieza26"
+    });
+
+    $("#pieza29").draggable();
+
+    //Posesionando la pieza 30 en pieza 27 y la propiedad de movimiento
+    $("#pieza30").position({
+        my:"left",
+        at:"right",
+        of:"#pieza27"
+    });
+
+    $("#pieza30").draggable();
+    //Posesionando la pieza 31 en pieza 28 y la propiedad de movimiento
+    $("#pieza31").position({
+        my:"left",
+        at:"right",
+        of:"#pieza28"
+    });
+
+    $("#pieza31").draggable();
+
+    //Posesionando la pieza 32 en pieza 29 y la propiedad de movimiento
+    $("#pieza32").position({
+        my:"left",
+        at:"right",
+        of:"#pieza29"
+    });
+
+    $("#pieza32").draggable();
+
+    //Posesionando la pieza 33 en pieza 30 y la propiedad de movimiento
+    $("#pieza33").position({
+        my:"left",
+        at:"right",
+        of:"#pieza30"
+    });
+
+    $("#pieza33").draggable();
+
+    //Posesionando la pieza 34 en pieza 31 y la propiedad de movimiento
+    $("#pieza34").position({
+        my:"left",
+        at:"right",
+        of:"#pieza31"
+    });
+
+    $("#pieza34").draggable();
+
+    //Posesionando la pieza 35 en pieza 32 y la propiedad de movimiento
+    $("#pieza35").position({
+        my:"left",
+        at:"right",
+        of:"#pieza32"
+    });
+
+    $("#pieza35").draggable();
+
+    //Posesionando la pieza 36 en pieza 33 y la propiedad de movimiento
+    $("#pieza36").position({
+        my:"left",
+        at:"right",
+        of:"#pieza33"
+    });
+
+    $("#pieza36").draggable();
+
+    //Posesionando la pieza 37 en pieza 34 y la propiedad de movimiento
+    $("#pieza37").position({
+        my:"left",
+        at:"right",
+        of:"#pieza34"
+    });
+
+    $("#pieza37").draggable();
+
+    //Posesionando la pieza 38 en pieza 35 y la propiedad de movimiento
+    $("#pieza38").position({
+        my:"left",
+        at:"right",
+        of:"#pieza35"
+    });
+
+    $("#pieza38").draggable();
+
+    //Posesionando la pieza 39 en pieza 36 y la propiedad de movimiento
+    $("#pieza39").position({
+        my:"left",
+        at:"right",
+        of:"#pieza36"
+    });
+
+    $("#pieza39").draggable();
+
 });
