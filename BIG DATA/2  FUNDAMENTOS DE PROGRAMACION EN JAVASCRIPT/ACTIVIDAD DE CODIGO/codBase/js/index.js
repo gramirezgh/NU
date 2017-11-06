e7238b60a8f4a86440ea8141d221ad4d25f8901f
gramirezgh <guerynextu@gmail.com>
2017-11-06 11:00:22 -0400

    javascript lo esencial BIG DATA
    javascript lo esencial BIG DATA

---
+++ BIG DATA/2  FUNDAMENTOS DE PROGRAMACION EN JAVASCRIPT/ACTIVIDAD DE CODIGO/codBase/js/index.js
@@ -0,0 +1,39 @@
+
+document.getElementById('boton-receta1').addEventListener('click', function(){
+var ingrediente_1 = "Claras de huevo";
+var cantidad_ing1 = 5;
+var ingrediente_2 = "Azucar Blanca";
+var cantidad_ing2 = 150;
+var ingrediente_3 = "Leche Condensada";
+var cantidad_ing3 = 200;
+var ingrediente_4 = "Gelatina sin Sabor";
+var cantidad_ing4 = 1;
+var ingrediente_5 = "Limones";
+var cantidad_ing5 = 4;
+
+console.log("Preparando....");
+console.log("Adicionando las "+cantidad_ing1+" unidades "+ ingrediente_1+ " en un Bol.");
+console.log("Batir los "+cantidad_ing2+ "[gr]"+" de "+ingrediente_2);
+console.log("Luego adicionar "+cantidad_ing3+" [gr] "+" de "+ingrediente_3+" batiendo bien");
+console.log("Una vez se termine con esto.");
+console.log("Adicione el zumo de "+ ingrediente_5+" y la "+ingrediente_4+" hidratada en agua tibia.");
+console.log("Por ultimo deposite la mezcla en moldes individuales y metelo a la nevera ");
+console.log("En 30 minutos to postre estara listo");
+console.log("A disfrutar!!!!");
+
+})
+
+document.getElementById('boton-receta2').addEventListener('click', function(){
+
+var ingrediente_1 = "ginebra";
+var cantidad_ing1 = 5;
+var ingrediente_2 = "Vermouth Seco";
+var cantidad_ing2 = "0.5";
+var ingrediente_3 = "aceituna";
+var cantidad_3 = 1;
+
+console.log("Para preparar un Martini básico debes verter el"+ingrediente_1+"en una coctelera");
+console.log("Luego añadir el "+ingrediente_2);
+console.log("SErvir en una copa tipo martini y añadir "+cantidad_3+" "+ingrediente_3);
+console.log("A farrear!!!!");
+})
