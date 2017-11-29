6d0672e6d7fe1cfe2007d90eaee3cdc8bd203bca
gramirezgh <guerynextu@gmail.com>
2017-11-28 22:25:36 -0400

    servicios
    servicios

--- DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/servicios.js
+++ DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/servicios.js
@@ -1,4 +1,22 @@
 'use strict';
 var realTemApp = angular.module('realTempAppServices',[]);
 
-realTemApp.factory
+realTemApp.factory('Temp',function () {
+    return {
+        celciusToFahrenheit: function (temp) {
+            var tempInFahrenheit = 0.0;
+            var tempInCelsius = temp;
+
+            tempInFahrenheit = tempInCelsius * 9 / 5 + 32;
+            return tempInFahrenheit;
+        },
+            celciusToKelvin: function (temp){
+                var tempInKelvin = 0.0;
+                var tempInFahrenheit = temp;
+
+                tempInKelvin = tempInCelcius -273.15;
+                return tempInKelvin;
+            
+        }
+    }
+});
