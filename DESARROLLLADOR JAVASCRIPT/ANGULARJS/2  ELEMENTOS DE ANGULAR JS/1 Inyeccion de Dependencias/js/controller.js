b1eab2d295c3074d8b9b65b56498bc8c2bfe5dc2
gramirezgh <guerynextu@gmail.com>
2017-11-30 11:11:08 -0400

    corrigiendo errores angular app
    corrigiendo errores angular app

--- DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/controller.js
+++ DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/controller.js
@@ -1,14 +1,14 @@
 ﻿'use strict';
 
-var tempApp = angular.module("tempAppcontroller", []);
+var tempApp = angular.module('tempAppcontroller', []);
 
-tempApp.controller('TempController', function (Temp, $scope) {
+tempApp.controller('TempController', function ($scope, Temp) {
     $scope.resultInFahrenheit = 0.0;
     $scope.resultInKelvin = 0.0;
     $scope.tempCelcius = 0.0;
 
     $scope.convertTemperatures = function () {
-        $scope.resultInFahrenheit = Temp.celcelciusToFahrenheit($scope.tempCelcius);
-        $scope.resultInKelvin = Temp.celcelciusToKelvin($scope.tempCelcius);
+        $scope.resultInFahrenheit = Temp.celciusToFahrenheit($scope.tempCelcius);
+        $scope.resultInKelvin = Temp.celciusToKelvin($scope.tempCelcius);
     }
 });
--- DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/servicios.js
+++ DESARROLLLADOR JAVASCRIPT/ANGULARJS/2  ELEMENTOS DE ANGULAR JS/1 Inyeccion de Dependencias/js/servicios.js
@@ -1,30 +1,5 @@
-//'use strict';
-//var realTemApp = angular.module('realTempAppServices',[]);
-
-//realTemApp.factory('Temp',function () {
-//    return {
-//        celciusToFahrenheit: function (temp) {
-//            var tempInFahrenheit = 0.0;
-//            var tempInCelsius = temp;
-
-//            tempInFahrenheit = tempInCelsius * 9 / 5 + 32;
-//            return tempInFahrenheit;
-//        },
-//            celciusToKelvin: function (temp){
-//                var tempInKelvin = 0.0;
-//                var tempInFahrenheit = temp;
-
-//                tempInKelvin = tempInCelcius -273.15;
-//                return tempInKelvin;
-            
-//        }
-//    }
-//});
-
-
-
 'use strict';
-var tempApp = angular.module("tempAppServices", []);
+var tempApp = angular.module('tempAppServices', []);
 
 tempApp.factory('Temp', function () {
     return {
