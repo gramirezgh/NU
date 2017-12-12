4df60faa6cba9e7f71c47546f53b098b2f77119c
gramirezgh <guerynextu@gmail.com>
2017-12-12 11:42:31 -0400

    esri api
    esri api

---
+++ ESRI API JAVASCRIPT/4.5/js/arcgis_js_v45_api/arcgis_js_api/library/4.5/esri/plugins/pe.js
@@ -0,0 +1,4 @@
+// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+// See https://js.arcgis.com/4.5/esri/copyright.txt for details.
+//>>built
+define(["require","exports","../core/global"],function(f,b,d){Object.defineProperty(b,"__esModule",{value:!0});b.load=function(b,e,c){e(["../geometry/support/"+("WebAssembly"in d?"pe-wasm":"pe-asmjs")],function(a){a.calledRun?c(a):a.onRuntimeInitialized=function(){c(a)}})}});
