063c0fb0293f31d685fbbe4680c67aaed718f077
gramirezgh <guerynextu@gmail.com>
2018-08-02 22:56:05 -0400

    manejo de consola nodejs
    manejo de consola nodejs

---
+++ DESARROLLLADOR JAVASCRIPT/NODEJS  I/1 INTRODUCCION NODEJS/1 INSTALACION Y HOLA MUNDO/imprimirCadena.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url  = require('url');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    var params = url.parse(request.url, true).query;
+    var nombre = params.nombre;
+    response.end('Hola '+nombre);
+
+}).listen(3000);
+console.log("servidor corriendo en http://127.0.0.1:3000");
